20ef4e50f167a7d8f032dc252289a35f16e82060
Shin-Sangseok <82014079+Shin-Sangseok@users.noreply.github.com>
2022-05-19 13:26:10 +0900

    추천시스템 RMSE 01

--- shop_dao_class1.py
+++ shop_dao_class1.py
@@ -1,4 +1,8 @@
 import pymysql
+import shop_use
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class DAO:
     conn = None
@@ -22,18 +26,71 @@ class DAO:
             print('db연결 중 에러 발생!!')
             print('에러정보>>', e)
 
-    def recom_read(self):
-        sql = "select p.product_idx , p.user_id , p.purchase_quantity , m.user_age " \
-              "from purchaseinfo p " \
-              "inner join `member` m " \
-              "on m.user_id  = p.user_id "
+ #   def recom_read(self):
+ #       sql = "select p.product_idx , p.user_id , p.purchase_quantity , m.user_age " \
+ #             "from purchaseinfo p " \
+ #             "inner join `member` m " \
+ #             "on m.user_id  = p.user_id "
+
+ #       result = self.cur.execute(sql)
+ #       print('sql문 전송 결과>', result)
+
+ #       #read인 경우 , 커서로 연결통로(스트림)에 검색결과를 꺼내주어야 한다
+ #       row = self.cur.fetchall()
+ #       print(row)
+ #       self.conn.close()
+ #       return row #검색결과 return
+
+    #멤버테이블 user_id, user_age
+    def member_read(self):
+        sql = "select user_id, user_age from member "
 
         result = self.cur.execute(sql)
         print('sql문 전송 결과>', result)
 
-        #read인 경우 , 커서로 연결통로(스트림)에 검색결과를 꺼내주어야 한다
+        #read인 경우, 커서로 연결통로(스트림)에 검색결과를 꺼내주어야 한다.
+        row = self.cur.fetchall()
+        print(row)
+     #   self.conn.close()
+        return row
+
+    #구매 테이블 user_id, product_idx, purchase_quantity
+
+    def purchase_read(self):
+        sql = "select user_id, product_idx, purchase_quantity from purchaseinfo"
+
+        result = self.cur.execute(sql)
+        print('sql문 전송 결과>', result)
+
+        #read인 경우, 커서로 연결통로(스트림)에 검색결과를 꺼내주어야 한다
         row = self.cur.fetchall()
         print(row)
         self.conn.close()
-        return row #검색결과 return
+        return row
+
+    #정확도(RMSE)를 계산하는 함수
+    def RMSE(self,y_true, y_pred):
+        return np.sqrt(np.mean((np.array(y_true) - np.array(y_pred))**2))
+
+    #모델별 RMSE를 계산하는 함수
+    def score(self,model):
+        id_pairs = zip(shop_use.x_test['user_id'], shop_use.x_test['product_idx'])
+        y_pred = np.array([model(user,movie) for (user,movie) in id_pairs])
+        y_true = np.array(shop_use.x_test['purchase_quantity'])
+        return self.RMSE(y_true, y_pred)
+
+    #평균으로 예측치 계산하는 모델
+    def predict_quantity(self,user_id,purchase_quantity):
+        try:
+            purchase_quantity = shop_use.train_mean[purchase_quantity]
+        except:
+            purchase_quantity = 3
+        return purchase_quantity
+
+
+
+
+
+
+
 
--- shop_use.py
+++ shop_use.py
@@ -1,15 +1,46 @@
 import pandas as pd
+import numpy as np
+import score as score
 
 import shop_dao_class1
-
+from sklearn.model_selection import train_test_split
 #from db import dao
 #from 패키지 import 모듈명
 #from 패키지명.모듈명 import 함수명 , 클래스명, *
 #-->함수()
 
 if __name__ == '__main__':
+
+    #shop_dao_class1.py
     dao = shop_dao_class1.DAO()
-    recom_result= dao.recom_read()
-    recom_columns = ['product_idx', 'user_id', 'purchase_quantity', 'user_age']
-    df = pd.DataFrame(data = recom_result, columns=recom_columns)
-    print(df)
+
+    #recom_result= dao.recom_read()
+    #recom_columns = ['product_idx', 'user_id', 'purchase_quantity', 'user_age']
+    #df = pd.DataFrame(data = recom_result, columns=recom_columns)
+    #print(df)
+
+    member_result = dao.member_read()
+    purchase_result = dao.purchase_read()
+
+    m_cols = ['user_id', 'user_age']
+    p_cols = ['user_id', 'product_idx', 'purchase_quantity']
+
+    #member dataFrame
+    member_df = pd.DataFrame(data = member_result, columns=m_cols)
+
+    #purchase_info dataFrame
+    purchase_df = pd.DataFrame(data = purchase_result, columns=p_cols)
+    print(member_df)
+    print(purchase_df)
+
+    x = purchase_df.copy()
+    y = purchase_df['user_id']
+
+    #각 user_id별 train set과 test set 비율 동일하게 유지
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.25)
+
+    train_mean = x_train.groupby(['user_id'])['purchase_quantity'].mean()
+    print(dao.predict_quantity)
+    score(dao.predict_quantity)
+
+
